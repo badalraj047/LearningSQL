77815262ac6f30bb764d47ba05f80c3624c606e3
Anshika <badalraj.047@gmail.com>
2025-12-28 19:49:26 +0530

    Update changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
+  server: {
+    host: true,
     allowedHosts: [
       "cipherschoolslearningsql.onrender.com"
     ]
